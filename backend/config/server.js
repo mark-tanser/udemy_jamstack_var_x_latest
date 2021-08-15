50fea52b00043dae9b87f271cfaa84b32e38c046
Mark Tanser <tanser.mark@gmail.com>
2021-08-15 13:03:36 +0900

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd7838250965d6c4a393e30122d2cbe8'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1ae47a5b-2b37-4919-b124-61461fbf8425'
+};
