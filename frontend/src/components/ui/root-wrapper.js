6fef0e19a92d0a59e504656a8840dd4f11f9f91c
Mark Tanser <tanser.mark@gmail.com>
2021-08-17 14:49:44 +0900

    fix warning: warning  Assign arrow function to a variable before exporting as module default-export

--- frontend/src/components/ui/root-wrapper.js
+++ frontend/src/components/ui/root-wrapper.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './theme'
 
+const element = ({ element }) => {
+    return (
+        <ThemeProvider theme={theme}>
+        {element}
+        </ThemeProvider>
+    )
+}
+
+export default element
+
+/* above replaces original code below which caused warning: Assign arrow function to a variable before exporting as module default
 export default ({ element }) => {
     return (
         <ThemeProvider theme={theme}>
@@ -9,3 +20,4 @@ export default ({ element }) => {
         </ThemeProvider>
     )
 }
+*/
