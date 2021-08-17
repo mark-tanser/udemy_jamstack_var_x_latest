adeaec46193433c96bf6ec8c023e607fc04c35c1
Mark Tanser <tanser.mark@gmail.com>
2021-08-17 15:24:10 +0900

    complete 43.Creating A Custom Theme Part 4

--- frontend/src/components/ui/theme.js
+++ frontend/src/components/ui/theme.js
@@ -1,26 +1,68 @@
 import { createTheme } from '@material-ui/core/styles'
 
+const green = "#99B898"
+const darkGreen = "#708670"
+const tan = "#FECEA8"
+const lightRed = "FF847C"
+const red = "#E84A5F"
+const offBlack = "#2A363B"
+const grey = "#747474"
+
+
 const theme = createTheme({
     palette: {
         primary: {
-            main: ""
+            main: green
         },
         secondary: {
-            main: ""
+            main: darkGreen
         },
         common: {
-            orange: "" //provide specific versions of your ad-hoc colors here
+            tan,
+            lightRed,
+            red,
+            offBlack
         }
     },
     typography: {
-        h1: {},
-        h2: {},
-        h3: {},
-        h4: {},
-        h5: {},
+        h1: {
+            fontFamily: "Philosopher",
+            fontSize: "4.5rem",
+            fontStyle: "italic",
+            fontWeight: 700,
+            color: green
+        },
+        h2: {
+            fontFamily: "Montserrat",
+            fontSize: "3rem",
+            fontWeight: 500,
+            color: "FFF"
+        },
+        h3: {
+            fontFamily: "Montserrat",
+            fontSize: "2rem",
+            fontWeight: 300
+        },
+        h4: {
+            fontFamily: "Philosopher",
+            fontSize: "3rem",
+            fontStyle: "italic",
+            fontWeight: 700,
+            color: "FFF"
+        },
+        h5: {
+            fontFamily: "Philosopher",
+            fontSize: "2rem",
+            fontStyle: "italic",
+            fontWeight: 700,
+            color: "FFF"
+        },
         h6: {},
-        body1: {},
-        body2: {}
+        body1: {
+            fontFamily: "Montserrat",
+            fontSize: "1.5rem",
+            color: grey
+        },
     },
     overrides: {}
 })
