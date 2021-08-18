8eab9ca2b52d1347dd0822f19dccd42ad3198596
Mark Tanser <tanser.mark@gmail.com>
2021-08-18 12:44:18 +0900

    Complete 46.Creating A Header Part 3

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -3,16 +3,37 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+
+import search from '../../images/search.svg'
+import cart from '../../images/cart.svg'
+import account from '../../images/account-header.svg'
+
 
 export default function Header() {
   return (
     <AppBar color="transparent" elevation={0}>
       <Toolbar>
         <Button>
-          <Typography variant="h1">var x</Typography>
+          <Typography variant="h1">VAR X</Typography>
         </Button>
+        <Tabs>
+          <Tab label="Hats"/>
+          <Tab label="Hoodies"/>
+          <Tab label="Shirts"/>
+          <Tab label="Contact Us"/>
+        </Tabs>
+        <IconButton>
+          <img src={search} alt="search" />
+        </IconButton>
+        <IconButton>
+          <img src={cart} alt="cart" />
+        </IconButton>
+        <IconButton>
+          <img src={account} alt="account" />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
