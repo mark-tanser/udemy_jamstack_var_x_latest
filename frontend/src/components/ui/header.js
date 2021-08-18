e4634ae5949e6ead62cebfdb800f784820d291df
Mark Tanser <tanser.mark@gmail.com>
2021-08-18 12:30:23 +0900

    Complete 45.Creating A Header Part 2

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -1,1 +1,19 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
+export default function Header() {
+  return (
+    <AppBar color="transparent" elevation={0}>
+      <Toolbar>
+        <Button>
+          <Typography variant="h1">var x</Typography>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
+}
