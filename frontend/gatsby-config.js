b2b7994b6f25d8936a0f0491989536a63b9fee98
Mark Tanser <tanser.mark@gmail.com>
2021-08-18 15:23:01 +0900

    Complete 49.Customizing MUI Components Part 2

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
+    /*{
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -46,6 +46,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    */
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
