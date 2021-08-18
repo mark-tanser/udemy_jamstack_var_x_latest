0de30fa03897943db60e5c4b5ae93357c074160f
Mark Tanser <tanser.mark@gmail.com>
2021-08-18 12:00:41 +0900

    Complete 45.Creating A Header Part 1

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,14 +1,15 @@
 import * as React from "react"
 
 import Layout from "../components/ui/layout"
-import Seo from "../components/ui/seo"
+//import Seo from "../components/ui/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
 export default NotFoundPage
+
+// deleted: <Seo title="404: Not found" />
