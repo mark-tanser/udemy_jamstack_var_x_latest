3850683109518b57ffa69b5665c4358c879759b5
Mark Tanser <tanser.mark@gmail.com>
2021-08-20 16:19:43 +0900

    Complete 68.Create Promo Carousel

--- frontend/src/components/home/PromotionalProducts.js
+++ frontend/src/components/home/PromotionalProducts.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import Carousel from 'react-spring-3d-carousel'
+import clsx from 'clsx'
 import { useStaticQuery, graphql } from 'gatsby'
+import { makeStyles } from '@material-ui/core/styles'
+
+import promoAdornment from '../../images/promo-adornment.svg'
+import explore from '../../images/explore.svg'
+
+const useStyles = makeStyles(theme => ({
+    mainContainer: {
+        backgroundImage: `url(${promoAdornment})`,
+        backgroundPosition: 'top',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '70rem',
+        padding: '30rem 10rem 10rem 10rem'
+    }
+}))
 
 export default function PromotionalProducts() {
+    const classes = useStyles()
+    const [selectedSlide, setSelectedSlide] = useState(0)
 
     const data = useStaticQuery(graphql`
     query MyQuery {
@@ -25,7 +47,29 @@ export default function PromotionalProducts() {
     }
     `)
 
+    var slides = [
+        {key: 1, content: <div>First Slide</div>},
+        {key: 2, content: <div>Second Slide</div>},
+        {key: 3, content: <div>Third Slide</div>}
+    ]
+
+    // data.allStrapiProduct.edges.map({ node } => )
+
     console.log(data)
 
-    return <div>Promo Products</div>
+    return (
+        <Grid 
+            container 
+            justifyContent="space-between" 
+            alignItems="center"
+            classes={{root: classes.mainContainer}}
+        >
+            <Grid item>
+                <Carousel slides={slides} goToSlide={selectedSlide} />
+            </Grid>
+            <Grid item>
+                Description
+            </Grid>
+        </Grid>    
+    )
 }
