6a65cd336701531b8d03d7016e511131043c6781
Mark Tanser <tanser.mark@gmail.com>
2021-08-23 12:51:08 +0900

    Complete 74.Create FeaturedProduct Slide

--- frontend/src/components/home/FeaturedProducts.js
+++ frontend/src/components/home/FeaturedProducts.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
 import { useStaticQuery, graphql } from 'gatsby'
+import { makeStyles } from '@material-ui/core/styles'
+
+import featuredAdornment from '../../images/featured-adornment.svg'
+import frame from '../../images/product-frame-grid.svg'
+import { mergeClasses } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    background: {
+        backgroundImage: `url(${featuredAdornment})`,
+        backgroundPosition: 'top',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '180rem',
+        padding: '0 2.5rem'
+    },
+    featured: {
+        height: '20rem',
+        width: '20rem',
+    },
+    frame: {
+        backgroundImage: `url(${frame})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        borderRadius: 0,
+        height: '24.8rem',
+        width: '25rem',
+        boxSizing: 'border-box',
+        boxShadow: theme.shadows[5],
+        position: 'absolute',
+        zIndex: 1
+    },
+    slide: {
+        backgroundColor: theme.palette.primary.main,
+        height: '20rem',
+        width: '24.5rem',
+        zIndex: 0,
+        transition: 'transform 0.5s ease'
+    },
+    slideLeft: {
+        transform: 'translate(-24.5rem, 0px)',
+    },
+    slideRight: {
+        transform: 'translate(24.5rem, 0px)',
+    },
+    productContainer: {
+        margin: '5rem 0'
+    }
+}))
 
 export default function FeaturedProducts() {
+    const classes = useStyles()
+    const [expanded, setExpanded] = useState(null)
+
     const data = useStaticQuery(graphql`
     query GetFeatured {
         allStrapiProduct(filter: {promo: {}, featured: {eq: true}, parent: {}}) {
@@ -23,5 +80,52 @@ export default function FeaturedProducts() {
 
     console.log(data)
 
-    return <div>Featured Products</div>
+    return (
+        <Grid 
+            container 
+            direction="column" 
+            justifyContent="center"
+            classes={{ root: classes.background }}
+        >
+            {data.allStrapiProduct.edges.map(({node}, i) => {
+                const alignment = i === 0 || i === 3 ? "flex-start" : i === 1 || i === 4 ? "center" : "flex-end"
+                return (
+                <Grid 
+                    item 
+                    container 
+                    justifyContent={alignment} 
+                    key={node.strapiId}
+                    classes={{ root: classes.productContainer }}
+                    alignItems="center"
+                >
+                        
+                            <IconButton 
+                                onClick={() => expanded === i ? setExpanded(null) : setExpanded(i)}
+                                classes={{ root: classes.frame }}
+                            >
+                                    <img 
+                                        src={
+                                            process.env.GATSBY_STRAPI_URL +
+                                            node.variants[0].images[0].url
+                                        }
+                                        alt={node.name}
+                                        className={classes.featured}
+                                    />
+                            </IconButton>
+                            <Grid 
+                                container 
+                                direction="column" 
+                                classes={{ root: clsx(classes.slide, {
+                                    [classes.slideLeft] : expanded === i && alignment === "flex-end",
+                                    [classes.slideRight] : expanded === i && (alignment === "flex-start" || alignment === "center")
+
+                                }) }}
+                            >
+
+                            </Grid>    
+                        
+                </Grid>
+            )})}
+        </Grid>
+    )
 }
