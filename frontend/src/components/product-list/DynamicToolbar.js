9290bf26464fda4b65f88d9ca24e6e1875622e46
Mark Tanser <tanser.mark@gmail.com>
2021-08-25 17:07:20 +0900

    Complete 108.Adding The Filter/Sort Buttons

--- frontend/src/components/product-list/DynamicToolbar.js
+++ frontend/src/components/product-list/DynamicToolbar.js
@@ -3,6 +3,8 @@ import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
 
+import FunctionContainer from './FunctionContainer'
+
 const useStyles = makeStyles(theme => ({
     toolbar: {
         border: `5px solid ${theme.palette.primary.main}`,
@@ -17,7 +19,7 @@ export default function DynamicToolbar() {
 
     return (
         <Grid item container direction="column" classes={{ root: classes.toolbar }}>
-
+            <FunctionContainer />
         </Grid>
     )
 }
