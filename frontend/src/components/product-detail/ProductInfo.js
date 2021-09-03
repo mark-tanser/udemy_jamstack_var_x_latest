0b8e1f9708b0781e5c126f344bb722d7bccc0b34
Mark Tanser <tanser.mark@gmail.com>
2021-09-03 13:29:23 +0900

    Complete 171.Product Detail Actions Part 2

--- frontend/src/components/product-detail/ProductInfo.js
+++ frontend/src/components/product-detail/ProductInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import clsx from 'clsx'
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
@@ -8,6 +8,10 @@ import Chip from "@material-ui/core/Chip"
 import { makeStyles } from "@material-ui/core/styles"
 
 import Rating from "../home/Rating"
+import Sizes from "../product-list/Sizes"
+import Swatches from '../product-list/Swatches'
+import QtyButton from '../product-list/QtyButton'
+import { colorIndex } from '../product-list/ProductFrameGrid'
 
 import favorite from '../../images/favorite.svg'
 import subscription from '../../images/subscription.svg'
@@ -59,10 +63,44 @@ const useStyles = makeStyles(theme => ({
     chipLabel: {
         fontSize: "2rem"
     },
+    stock: {
+        color: "#fff",
+    },
+    sizesAndSwatches: {
+        maxWidth: "13rem",
+    },
+    actionsContainer: {
+        padding: "0 1rem",
+    },
+    "@global": {
+        ".MuiButtonGroup-groupedOutlinedVertical:not(:first-child)": {
+            marginTop: 0,
+        }
+    },
 }))
 
 export default function ProductInfo({ name, description, variants, selectedVariant, setSelectedVariant}) {
     const classes = useStyles()
+    const [selectedSize, setSelectedSize] = useState(null)
+    const [selectedColor, setSelectedColor] = useState(null)
+
+    const imageIndex = colorIndex({ node: { variants }}, variants[selectedVariant], selectedColor)
+
+    const sizes = []
+    const colors = []
+    variants.map(variant => {
+        sizes.push(variant.size)
+
+        if (!colors.includes(variant.color)) {
+            colors.push(variant.color)
+        }
+    })
+
+    useEffect(() => {
+        if (imageIndex !== -1) {
+            setSelectedVariant(imageIndex)
+        }
+    }, [imageIndex])
 
     return (
         <Grid 
@@ -122,8 +160,30 @@ export default function ProductInfo({ name, description, variants, selectedVaria
                     </Grid>
                 </Grid>
 
-                <Grid item container classes={{ root: classes.sectionContainer }}>
-                    
+                <Grid 
+                    item 
+                    container 
+                    justifyContent="space-between"
+                    alignItems="center"
+                    classes={{ root: clsx(classes.actionsContainer, classes.sectionContainer) }}
+                >
+
+                    <Grid item>
+                        <Grid container direction="column">
+                            <Grid item classes={{ root: classes.sizesAndSwatches }}>
+                                <Sizes sizes={sizes} selectedSize={selectedSize} setSelectedSize={setSelectedSize}/>
+                                <Swatches colors={colors} selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h3" classes={{ root: classes.stock }}>12 Currently in Stock</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                    <Grid item>
+                        <QtyButton />
+                    </Grid>
+
                 </Grid>
             </Grid>
             
