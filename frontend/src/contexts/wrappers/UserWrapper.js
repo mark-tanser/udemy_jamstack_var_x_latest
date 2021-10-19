4f178e3d584a904b5f910b7dd550d3822c19cee3
Mark Tanser <tanser.mark@gmail.com>
2021-10-19 12:03:14 +0900

    resolve merge conflicts

--- frontend/src/contexts/wrappers/UserWrapper.js
+++ frontend/src/contexts/wrappers/UserWrapper.js
@@ -2,10 +2,6 @@ import React, { useReducer, createContext } from "react"
 import userReducer from "../reducers/user-reducer"
 import { setUser } from "../actions"
 
-<<<<<<< HEAD
-=======
-
->>>>>>> afa4a13da444162954a3aafdc55d3ec62118da9b
 export const UserContext = createContext()
 const UserProvider = UserContext.Provider
 
@@ -18,8 +14,4 @@ export function UserWrapper({ children }) {
             {children}
         </UserProvider>
     )
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> afa4a13da444162954a3aafdc55d3ec62118da9b
