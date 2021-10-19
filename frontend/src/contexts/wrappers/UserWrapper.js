22957b8c8576dba72190f7f5f346fa8c108d25c2
Mark Tanser <tanser.mark@gmail.com>
2021-10-19 12:46:22 +0900

    Complete 218 Handling Tiken Expiration

--- frontend/src/contexts/wrappers/UserWrapper.js
+++ frontend/src/contexts/wrappers/UserWrapper.js
@@ -1,4 +1,5 @@
-import React, { useReducer, createContext } from "react"
+import React, { useEffect, useReducer, createContext } from "react"
+import axios from "axios"
 import userReducer from "../reducers/user-reducer"
 import { setUser } from "../actions"
 
@@ -9,6 +10,26 @@ export function UserWrapper({ children }) {
     const defaultUser = { username: "Guest" }
     const storedUser = JSON.parse(localStorage.getItem("user"))
     const [user, dispatchUser] = useReducer(userReducer, storedUser || defaultUser)
+
+    useEffect(() => {
+        if (storedUser) {
+            setTimeout(() => {
+                axios
+                    .get(process.env.GATSBY_STRAPI_URL + "/users/me", {
+                        headers: {
+                            Authorization: `Bearer ${storedUser.jwt}`
+                        }
+                    })
+                    .then(response => {
+                        dispatchUser(setUser({ ...response.data, jwt: storedUser.jwt}))
+                    })
+                    .catch(error => {
+                        console.error(error)
+                        dispatchUser(setUser(defaultUser))
+                    })
+            }, 3000)
+        }
+    }, [])
 
     return (
         <UserProvider value={{ user, dispatchUser, defaultUser }}>
