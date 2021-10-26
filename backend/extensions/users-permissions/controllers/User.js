410571eff143aec8c329f1669f193671a3aa7fb6
Mark Tanser <tanser.mark@gmail.com>
2021-10-26 14:49:08 +0900

    complete 266. Custom Controllers Part 2

---
+++ backend/extensions/users-permissions/controllers/User.js
@@ -0,0 +1,30 @@
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizeUser = user =>
+    sanitizeEntity(user, {
+        model: SVGAnimatedPreserveAspectRatio.query("user", "users-permissions").model,
+    });
+
+    module.exports = {
+        async setSettings(ctx) {
+            const { id, contactInfo, locations } = ctx.state.user
+            const { details, detailsSlot, location, locationSlot } = ctx.request.body
+
+            let newInfo = [...contactInfo]
+            let newLocations = [...locations]
+
+            if (typeof details !== "undefined" && typeof detailsSlot !== "undefined") {
+                newInfo[detailsSlot] = details
+            }
+
+            if (typeof location !== "undefined" && typeof locationSlot !== "undefined") {
+                newInfo[locationSlot] = location
+            }
+
+            let newUser = await strapi.plugins["users-permissions"].services.user.edit({ id }, { contactInfo:newInfo, locations: newLocations })
+
+            newUser = sanitizeUser(newUser)
+
+            ctx.send(newUser, 200)
+        }
+    }
