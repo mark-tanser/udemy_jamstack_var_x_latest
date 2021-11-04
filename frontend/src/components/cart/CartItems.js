14bd3f27d614791774e4495b002e301b4f6b6f12
Mark Tanser <tanser.mark@gmail.com>
2021-11-04 10:41:24 +0900

    complete 288. Create Cart Page

---
+++ frontend/src/components/cart/CartItems.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+import clsx from 'clsx'
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function CartItems() {
+    const classes = useStyles()
+
+    return (
+        <Grid>
+            
+        </Grid>
+    )
+}
--- frontend/src/pages/cart.js
+++ frontend/src/pages/cart.js
@@ -1,18 +1,38 @@
-import * as React from "react"
-//import { Link } from "gatsby"
-//import { StaticImage } from "gatsby-plugin-image"
+import React, { useState, useContext } from "react"
+import clsx from 'clsx'
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+
+import { makeStyles } from "@material-ui/core/styles"
+import { UserContext } from "../contexts"
 
 import Layout from "../components/ui/layout"
-//import Seo from "../components/ui/seo"
+import CheckoutPortal from "../components/cart/CheckoutPortal"
+import CartItems from "../components/cart/CartItems"
 
-const CartPage = () => (
-  <Layout>
+const useStyles = makeStyles(theme => ({
 
-    <h1>Cart</h1>
-    <p>Coming Soon></p>
-    
-    
-  </Layout>
-)
+}))
 
-export default CartPage
+export default function Cart() {
+    const classes = useStyles()
+    const { user } = useContext(UserContext)
+
+    return (
+        <Layout>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <Typography variant="h1">
+                {user.username}'s Cart
+              </Typography>
+              <Grid item container>
+                <CartItems />
+                <CheckoutPortal />
+              </Grid>  
+            </Grid>
+          </Grid>
+        </Layout>
+    )
+}
