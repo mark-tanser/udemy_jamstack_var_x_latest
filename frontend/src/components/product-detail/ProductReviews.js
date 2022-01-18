4e5c3ddd3ffbabedac05cb6f5b95d9c2a6dd3ffd
Mark Tanser <tanser.mark@gmail.com>
2022-01-18 12:27:33 +0900

    observe 400 Bad Request error in section 391. Get Reviews

--- frontend/src/components/product-detail/ProductReviews.js
+++ frontend/src/components/product-detail/ProductReviews.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react"
-import clsx from 'clsx'
+import React, { useState, useEffect } from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import { makeStyles } from "@material-ui/core/styles"
+import { useQuery } from "@apollo/client"
 
 import ProductReview from "./ProductReview"
+import { GET_REVIEWS } from "../../apollo/queries"
 
 const useStyles = makeStyles(theme => ({
     reviews: {
@@ -14,8 +15,19 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-export default function ProductReviews( {product} ) {
+export default function ProductReviews( { product } ) {
     const classes = useStyles()
+    const [reviews, setReviews] = useState([])
+
+    const { data } = useQuery(GET_REVIEWS, { variables: { id: product } })
+
+    useEffect(() => {
+      if (data) {
+        setReviews(data.product.reviews)
+      }
+    }, [data])
+
+    console.log(reviews)
 
     return (
         <Grid item container direction="column" classes={{ root: classes.reviews }}>
