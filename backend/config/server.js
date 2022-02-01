ddde76688d0ca2756ac6ec8065d25e1055ddccf1
Mark Tanser <tanser.mark@gmail.com>
2022-02-01 07:45:50 +0000

    Update server config for NGINX

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   cron: { enabled: true },
+  url: env("URL", "http://localhost"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '16824c0bbada9bdf8c548d536562410c'),
