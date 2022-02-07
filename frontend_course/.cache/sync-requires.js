3141993118bea2c2e4f400c36abbed7b435c85a4
Mark Tanser <tanser.mark@gmail.com>
2022-02-07 16:43:22 +0900

    reconfigure ec2 and s3

---
+++ frontend_course/.cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/pages/404.js"))),
+  "component---src-pages-account-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/pages/account.js"))),
+  "component---src-pages-cart-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/pages/cart.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/pages/index.js"))),
+  "component---src-templates-product-detail-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/templates/ProductDetail.js"))),
+  "component---src-templates-product-list-js": hot(preferDefault(require("/Users/marktanser/Dropbox/DEV/tutorials/courses/jamstack/var_x_latest/frontend_course/src/templates/ProductList.js")))
+}
+
