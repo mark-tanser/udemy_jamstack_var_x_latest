64d0a55cbe516fbb150420325ca02d583c891a7d
Mark Tanser <tanser.mark@gmail.com>
2022-02-18 10:15:18 +0900

    removed () in gatsbyImage query

--- frontend/src/components/home/PromotionalProducts.js
+++ frontend/src/components/home/PromotionalProducts.js
@@ -101,7 +101,7 @@ export default function PromotionalProducts() {
                             images {
                                 localFile {
                                     childImageSharp {
-                                        gatsbyImageData()
+                                        gatsbyImageData
                                     }
                                 }
                             }
